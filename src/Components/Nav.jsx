85d82878f5ec964b9f5c0851f5fb1c7c20e58bc1
PRIYANHSU2112 <sahujipriyanshu2112@gmail.com>
2025-10-06 15:11:10 +0530

    update

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -7,9 +7,9 @@ const Nav = () => {
   return (
     <>
     
-      <div className="nav fixed top-0 z-10 w-[100vw] min-h-[15vh] bg-[#D5DDE6] shadow-2xl shadow-[#D5DDE6]  flex items-center   justify-between px-2 md:px-10">
+      <div className="nav fixed top-0 z-[999] w-[100vw] min-h-[15vh] bg-[#375f8836]  flex items-center backdrop-blur-lg  justify-between px-2 md:px-10">
         <div className=" relative w-[40vw] h-[18vw] md:w-[15vw] md:h-[12vh]  rounded-md "><img className="w-full h-full object-cover  relative top-0" src={logo} alt="logo.png" /></div>
-        <div className="sec gap-4 text-xl  font-semibold hidden md:flex text-[#000000b8] " >
+        <div className="sec gap-4 text-xl font-semibold hidden md:flex text-[#000000b8] ">
           <NavLink 
             to="/"
             // style={({ isActive }) => ({ color: isActive ? "red" : "black" })}
